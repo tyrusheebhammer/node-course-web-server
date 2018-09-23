03680e2c87faade09245832137e22777b16d3009
Tyrus Sonneborn <tyrus.b.sonneborn@gmail.com>
2018-09-23 13:35:16 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express'); //#1
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express(); //#2
+
+var maintenance = false;
+//Middleware to tweak the way express works
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();    
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+
+    fs.appendFile('server.log', log + '\n', (err)=> {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+}); //register middleware
+
+
+if (maintenance){  
+    app.use((req, res, next) => {
+        res.render('maintenance.hbs');       
+    });
+}
+
+app.use(express.static(__dirname + '/public')); //#3
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => { // '/' is the root route
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website!',
+        name: 'Tyrus',
+        likes: ["dogs",
+                " cats",
+                " kayla"]
+    });
+}); 
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Bad Request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000')
+}); //developing locally .. #4
+
+//this allows you to serve up a directory in 4 lines above
